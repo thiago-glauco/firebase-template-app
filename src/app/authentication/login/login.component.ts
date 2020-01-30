21efb6b816ae217e3f2616b050cb3f0bba307f97
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-01-30 10:16:03 -0300

    2 files modified

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '../../services/authentication.service';
+import {User} from '../../shared/user';
+import {RegisteredUser} from '../../shared/registered-user';
+import {Observable, from} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  hidePassword: boolean = true; //just for the user interface
+  loggedUser: boolean = false; //toogle loggout and register buttons
+  verifiedUser: boolean; //toogle loggout and register buttons
+  passwordsEqual: boolean = true;
 
-  constructor() { }
+  loginUser: RegisteredUser = new RegisteredUser(); //object with user form data
+  registeredUser: firebase.User; //User with data returned from firebase auth service
+
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
   }
