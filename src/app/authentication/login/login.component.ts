c398f3331d48fc00a4a98a694a5fc7864113820c
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-01-30 16:58:04 -0300

    2 files modified

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -18,19 +18,6 @@ export class LoginComponent implements OnInit {
   loginUser: RegisteredUser = new RegisteredUser(); //object with user form data
   registeredUser: firebase.User; //User with data returned from firebase auth service
 
-  loginObject = {
-    next(userData){
-        console.log(userData)
-        this.registeredUser = this.authService.getCurrentUser();
-        this.loggedUser = true;
-        this.verifiedUser = this.registeredUser.emailVerified;
-    },
-    error(err) {
-      console.log(err);
-      this.loggedUser = false;
-    }
-  }
-
   constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
@@ -38,15 +25,38 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
-    console.log(this.loginUser.email)
-    this.authService.emailSignIn(this.loginUser.email, this.loginUser.password).subscribe(this.loginObject)
+    console.log(this.loginUser.email);
+    let that = this;
+    this.authService.emailSignIn(this.loginUser.email, this.loginUser.password).subscribe({
+      next(userData){
+        console.log(userData)
+        that.registeredUser = that.authService.getCurrentUser();
+        that.loggedUser = true;
+        that.verifiedUser = that.registeredUser.emailVerified;
+    },
+    error(err) {
+      console.log(err);
+      that.loggedUser = false;
+    }
+    })
   }
 
   googleLogin( ) {
     let that = this;
     //uses googl provider - easy with firebase
     const userCredential = this.authService.googleLogin( );
-    userCredential.subscribe(this.loginObject);
+    userCredential.subscribe({
+      next(userData){
+        console.log(userData)
+        that.registeredUser = that.authService.getCurrentUser();
+        that.loggedUser = true;
+        that.verifiedUser = that.registeredUser.emailVerified;
+    },
+    error(err) {
+      console.log(err);
+      that.loggedUser = false;
+    }
+    });
   }
 
   logout( ) {
