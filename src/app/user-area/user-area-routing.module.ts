f222a762774c51799c8fee6364330d6b77d6ce5b
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-02-03 13:26:19 -0300

    2 files modified

--- src/app/user-area/user-area-routing.module.ts
+++ src/app/user-area/user-area-routing.module.ts
@@ -5,7 +5,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'user',
     component: UserProfileComponent
   },
 ];
