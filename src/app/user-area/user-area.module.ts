8230d747b8be297baf9019b71b9d3fa18eb26f0d
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-02-13 09:59:34 -0300

    3 files modified

--- src/app/user-area/user-area.module.ts
+++ src/app/user-area/user-area.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserAreaRoutingModule } from './user-area-routing.module';
 import {AuthenticationGuard} from '../services/authentication.guard';
@@ -9,17 +10,21 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     UserAreaRoutingModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatMenuModule,
     FlexLayoutModule
   ],
   declarations: [UserProfileComponent],
--- src/app/user-area/user-profile/user-profile.component.ts
+++ src/app/user-area/user-profile/user-profile.component.ts
@@ -12,23 +12,29 @@ import { Observable } from 'rxjs';
 
 
 export class UserProfileComponent implements OnInit {
-  userData: DatabaseUser;
+
+  //userData just contains data extracted from observables
+  userData: DatabaseUser = {};
   userDataObservable: Observable<DatabaseUser>;
   fbUser: firebase.User;
 
   constructor(private userService: UserService,
     private authService: AuthenticationService,
   ) {
+    //gets authservice user data, and uses this data to recover the profile from the database
     this.fbUser = this.authService.getCurrentUser( );
     this.userDataObservable = this.userService.getDatabaseUser();
   }
 
   ngOnInit() {
+    //checks if user exist in database and load its data
     let that = this;
     this.userService.userExist(this.fbUser.uid);
     this.userDataObservable.subscribe({
       next(user){
-        that.userData = user},
+        that.userData = user
+        console.log( that.userData )
+        },
       error(err){ alert(err) }
     })
   }
