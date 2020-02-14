86b56c43eb29c1e18d1ce86eaaf4228f863c593a
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-02-14 18:34:23 -0300

    3 files modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home-component';
 
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
     path: 'user',
     loadChildren: () => import('./user-area/user-area.module').then(mod => mod.UserAreaModule)
   },
+    {
+      path: 'home', component: "HomeComponent"
+      }
+
 
 ];
 
