334c1cebc05b1c5764fafb7864bbfed41a9aee79
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-02-17 14:32:29 -0300

    about - error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about-component';
+import { AboutComponent } from './about/aboutcomponent';
 
 const routes: Routes = [
   {
     path: 'home', component: HomeComponent
   },
   {
-    path: 'About', component: AboutComponent
+    path: 'about', component: AboutComponent
   },
   {
     path: '', redirectTo: '/home', pathMatch: 'full'
