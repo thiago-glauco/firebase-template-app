901c2633267fbfb8256f01e0ea8f9a678f88082d
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-02-18 12:44:03 -0300

    1 file modified

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
+import { Routes, RouterModule} from '@angular/route';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
+
   }
+
+  back(){
+    this.location.back()
+  }
+
 
 }
