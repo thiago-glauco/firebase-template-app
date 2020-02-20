7dc8f5279e23aac890843a3d678955aaca047b2d
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-02-20 16:02:12 -0300

    3 files added

---
+++ src/app/vendas/vendas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrcamentoComponent } from './orcamento/orcamento.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OrcamentoComponent]
+})
+export class VendasModule { }
